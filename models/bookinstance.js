417b7d72bd2784398d6bb8ceca4d83920d40e6d6
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-06-10 17:52:52 +0800

    Book Instance update feature complete

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -23,5 +23,11 @@ BookInstanceSchema
     return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema
+.virtual("due_back_form")
+.get(function() {
+    return DateTime.fromJSDate(this.due_back).toISODate();
+});
+
 // Export model
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
